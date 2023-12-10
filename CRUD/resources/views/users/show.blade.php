e0d5790dc3d1a4373d7e18e117f3473b312cba54
YousefSaleh1 <ys9882111@gmail.com>
2023-12-10 18:27:08 +0200

    project

---
+++ CRUD/resources/views/users/show.blade.php
@@ -0,0 +1,29 @@
+@extends('users.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show user</h2>
+            </div>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-end"
+                <a class=" btn btn-primary" href="{{ route('users.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                {{ $user->name }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Email:</strong>
+                {{ $user->email }}
+            </div>
+        </div>
+    </div>
+@endsection
